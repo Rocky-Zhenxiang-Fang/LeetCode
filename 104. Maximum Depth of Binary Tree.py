a0eec50772376108eda0e05dbdf37f362c753e2b
RockyZhenxiang <rockys910109@gmail.com>
2020-12-03 20:51:45 -0800

    Finished 104

---
+++ 104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+from LeetCode.DS import TreeNode
+
+
+    class Solution:
+        def maxDepth(self, root: TreeNode) -> int:
+            if not root: return 0
+            depth = 0
+
+            que = deque([root])
+            while que:
+                n = len(que)
+                for i in range(len(que)):
+                    node = que.popleft()
+                    if node.left:
+                        que.append(node.left)
+                    if node.right:
+                        que.append(node.right)
+                depth += 1
+
+            return depth
