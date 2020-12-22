608fd6462c4772032a3b6424c9d7ca8c0724843f
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-12-22 14:17:52 -0800

    Fixed 322

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -3,18 +3,13 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [[float("inf") for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
-        for row in dp:
-            row[0] = 0
-        for row in range(1, len(dp)):
-            curr = coins[row - 1]
-            for col in range(1, len(dp[0])):
-                if col >= curr:
-                    dp[row][col] = min(dp[row - 1][col], dp[row][col - curr] + 1)
-                else:
-                    dp[row][col] = dp[row - 1][col]
-
-        return dp[-1][-1] if dp[-1][-1] != float("inf") else -1
+        dp = [float("inf") for _ in range(amount + 1)]
+        dp[0] = 0
+        for i in range(1, len(dp)):
+            for c in coins:
+                if i >= c:
+                    dp[i] = min(dp[i], dp[i - c] + 1)
+        return -1 if dp[-1] == float("inf") else dp[-1]
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -7,18 +7,18 @@ import DS
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        lowest_price = float("inf")
-        max_profit = 0
-        if not prices:
-            return 0
-        for p in prices:
-            max_profit = max(max_profit, p - lowest_price)
-            lowest_price = min(lowest_price, p)
-        return max_profit
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf") for _ in range(amount + 1)]
+        dp[0] = 0
+        for i in range(1, len(dp)):
+            for c in coins:
+                if i >= c:
+                    dp[i] = min(dp[i], dp[i - c] + 1)
+        return -1 if dp[-1] == float("inf") else dp[-1]
 
 
 if __name__ == '__main__':
-    nums = [7,6,4,3,1]
+    nums = [1, 2, 5]
+    target = 11
     sol = Solution()
-    print(sol.maxProfit(nums))
+    print(sol.coinChange(nums, target))
