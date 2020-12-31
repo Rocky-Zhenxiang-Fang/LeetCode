477004de85837d3f18f2eb3958bed06f1c0037d7
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-12-31 10:27:10 -0800

    42

--- 560. Subarray Sum Equals K.py
+++ 560. Subarray Sum Equals K.py
@@ -4,27 +4,31 @@ from typing import List
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         """
-        From https://leetcode.com/problems/subarray-sum-equals-k/discuss/190674/Python-O(n)-Based-on-%22running_sum%22-concept-of-%22Cracking-the-coding-interview%22-book
-        Idea: On each element of
-        nums, see if the subarray from start to itself can form the answer, also check if the answer can be form by
-        subtracting a subarray from the start to another point, if can, add the number of its apperance
+        Idea: When adding a new number, first check the entire array nums[:i]
+            Then, see if this array can be decided into two parts, nums[:j] + nums[j + 1:k]
+            Where sum(nums[:j]) + sum(nums[j + 1 : k]) == k
+            Since nums[:J] will be visited earlier, we can store its sum in a map, then treat it
+            as a Two Sum problem
+            Note:
+                Can use running sum instead of sum() to get a better runtime
         """
-        running_sum = 0
-        res = 0
         seen = {}
-        for n in nums:
-            running_sum += n
-            if running_sum == k:
+        running = 0
+        res = 0
+        for i in range(len(nums)):
+            running += nums[i]
+            if running == k:
                 res += 1
-            if running_sum - k in seen:
-                res += seen[running_sum - k]
-            seen[running_sum] = seen.get(running_sum, 0) + 1
+            if running - k in seen:
+                res += seen[running - k]
+            seen[running] = seen.get(running, 0) + 1
+
         return res
 
 
 if __name__ == '__main__':
-    nums = [1, 1, 1]
-    k = 2
+    nums = [1, 2, 3]
+    k = 3
     sol = Solution()
     print(sol.subarraySum(nums, k))
 
--- test.py
+++ test.py
@@ -7,42 +7,32 @@ import DS
 
 
 class Solution:
-    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
-        res = []
-        decimal = set()
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        """
+        Idea: When adding a new number, first check the entire array nums[:i]
+            Then, see if this array can be decided into two parts, nums[:j] + nums[j + 1:k]
+            Where sum(nums[:j]) + sum(nums[j + 1 : k]) == k
+            Since nums[:J] will be visited earlier, we can store its sum in a map, then treat it
+            as a Two Sum problem
+            Note:
+                Can use running sum instead of sum() to get a better runtime
+        """
+        seen = {}
+        running = 0
+        res = 0
+        for i in range(len(nums)):
+            running += nums[i]
+            if running == k:
+                res += 1
+            if running - k in seen:
+                res += seen[running - k]
+            seen[running] = seen.get(running, 0) + 1
 
-        def util(reminder: int) -> None:
-            if reminder == 0:
-                return
-            else:
-                if reminder >= denominator:
-                    next_remin = reminder % denominator
-                    if decimal:
-                        if reminder // denominator in decimal:
-                            res.insert(res.index(reminder // denominator), "(")
-                            res.append(")")
-                            return
-                        else:
-                            decimal.add(reminder // denominator)
-                    res.append(str(reminder // denominator))
-                else:
-                    next_remin = reminder
-                    if not decimal:
-                        decimal.add(-1)
-                        res.append("0.")
-                    else:
-                        res.append("0")
-                        decimal.add(0)
-                if decimal:
-                    next_remin = next_remin * 10
-            util(next_remin)
-
-        util(numerator)
-        return "".join(res)
+        return res
 
 
 if __name__ == '__main__':
-    nums = 4
-    den = 333
+    nums = [1, 2, 3]
+    k = 3
     sol = Solution()
-    print(sol.fractionToDecimal(nums, den))
+    print(sol.subarraySum(nums, k))
