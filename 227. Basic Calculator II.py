71b3ccdf74d11f857238ebafc9678b59695ed567
RockyZhenxiang <rockys910109@gmail.com>
2021-01-17 13:43:16 -0800

    227

--- 227. Basic Calculator II.py
+++ 227. Basic Calculator II.py
@@ -1,26 +1,28 @@
 class Solution:
     def calculate(self, s):
-        stack, num, sign = [], 0, '+'
-
+        """
+        Idea:
+            We want to do two rounds, first for * /, second for +-. The second can be done by using sum(stack)
+            Keep a stack of all integers, return the sum of stack
+            if operator is +-, push num or -num in stack
+            if operator is */, before pushing it into the stack, we need to combine it with the last element
+        """
+        stack, num, sign = [], 0, "+"
         for i in range(len(s)):
             if s[i].isdigit():
-                num = (num * 10) + int(s[i])
-            if s[i] in '+-*/' or i == len(s) - 1:
-                if sign == '+':
+                num = num * 10 + int(s[i])
+            if s[i] in {"+", "-", "*", "/"} or i == len(s) - 1:
+                if sign == "+":
                     stack.append(num)
-                if sign == '-':
+                elif sign == "-":
                     stack.append(-num)
-                if sign == '*':
+                elif sign == "*":
                     stack.append(stack.pop() * num)
-                if sign == '/':
-                    p = stack.pop()
-                    res = abs(p) // num
-                    stack.append(res if p >= 0 else -res)
+                elif sign == "/":       # python // is floor, but we want it truncate to 0
+                    stack.append(int(stack.pop()/num))
                 num = 0
                 sign = s[i]
-
         return sum(stack)
-
 
 
 if __name__ == '__main__':
--- 863. All Nodes Distance K in Binary Tree.py
+++ 863. All Nodes Distance K in Binary Tree.py
@@ -9,40 +9,29 @@ class Solution:
         Idea:
             if we recorded the parent of the node, this tree will turn into a graph, then do BFS in range K
         """
-        parent = {}
+        parents = {}
         res = []
+        visited = set()
 
         def preorder(node: TreeNode):
             if node.left:
-                parent[node.left] = node
+                parents[node.left] = node
                 preorder(node.left)
             if node.right:
-                parent[node.right] = node
+                parents[node.right] = node
                 preorder(node.right)
 
-        def bfs(node: TreeNode):
-            from collections import deque
-            que = deque()
-            que.append((node, K))
-            visited = set()
-            while que:
-                curr_node, k = que.pop()
-                if curr_node not in visited:
-                    visited.add(curr_node)
-                    if k == 0:
-                        res.append(curr_node.val)
-                        continue
-                    else:
-                        if curr_node.left:
-                            que.appendleft((curr_node.left, k - 1))
-                        if curr_node.right:
-                            que.appendleft((curr_node.right, k - 1))
-                        if curr_node in parent:
-                            que.appendleft((parent[curr_node], k - 1))
+        def dfs(node: TreeNode, k):
+            if not node or node in visited:
+                return
+            visited.add(node)
+            if k == 0:
+                res.append(node.val)
+            else:
+                dfs(node.right, k - 1)
+                dfs(node.left, k - 1)
+                dfs(parents.get(node, None), k - 1)
         preorder(root)
-        bfs(target)
+        dfs(target, K)
         return res
-
-
-
 
