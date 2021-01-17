70715e82378072a72bf150261cf7ffbe2d42ee41
RockyZhenxiang <rockys910109@gmail.com>
2021-01-17 14:04:45 -0800

    636

--- 636. Exclusive Time of Functions.py
+++ 636. Exclusive Time of Functions.py
@@ -5,21 +5,25 @@ class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         """
         Idea:
-            Since it only have one core, anytime a process is replace, the time it used can be added on
+            1. Since it only have one core, anytime a process is replace, the time it used can be added on
+            2. We can use a stack to save any tasks that is in background
+            3. We need to treat start and end differently based on the time
         """
-        log_list = [l.split(":") for l in logs]
-        res = [0 for _ in range(n)]
-        task = -1
-        time = 0
-        for log in log_list:
-            if task != -1:
-                res[task] += int(log[2]) - time
-                if log[1] == "end":
-                    res[task] += 1
-                    time += 1
-            task = int(log[0])
-            time = int(log[2])
-        return res
+        ans = [0 for _ in range(n)]
+        time = 0  # indicates the time of start
+        stack = []
+        for l in logs:
+            name, typ, t = l.split(":")
+            if typ == "start":
+                if stack:
+                    ans[int(stack[-1])] += int(t) - time
+                time = int(t)
+                stack.append(name)
+            else:
+                ans[int(stack.pop())] += int(t) - time + 1
+                time = int(t) + 1
+        return ans
+
 
 
 if __name__ == '__main__':
