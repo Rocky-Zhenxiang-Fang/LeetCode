e0d829a9b080496f81fe67e00da0a324caec0bab
RockyZhenxiang <rockys910109@gmail.com>
2021-02-17 20:12:38 -0800

    833

--- 833. Find And Replace in String.py
+++ 833. Find And Replace in String.py
@@ -4,15 +4,9 @@ from typing import List
 class Solution:
     def findReplaceString(self, S: str, indexes: List[int], sources: List[str], targets: List[str]) -> str:
         data = sorted(zip(indexes, sources, targets), key=lambda x: -x[0])
-        res = []
-        i = 0
-        while i < len(S):
-            if data and i == data[-1][0]:
-                id, source, target = data.pop()
-
-
-            else:
-                res.append(S[i])
-                i += 1
-
+        res = list(S)
+        for d in data:
+            id, source, target = d[0], d[1], d[2]
+            if S[id:id+len(source)] == source:
+                res = res[:id] + list(target) + res[id + len(source):]
         return "".join(res)
