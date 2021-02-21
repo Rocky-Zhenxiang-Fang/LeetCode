283d8ace36771a047fc987fee84e9055fd30add7
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2021-02-20 16:39:18 -0800

    652

--- 652. Find Duplicate Subtrees.py
+++ 652. Find Duplicate Subtrees.py
@@ -1,4 +1,3 @@
-import collections
 from typing import List
 import DS
 
@@ -6,18 +5,27 @@ from DS import TreeNode
 
 
 class Solution:
-    def findDuplicateSubtrees(self, root):
-        def getid(root):
-            if root:
-                id = treeid[root.val, getid(root.left), getid(root.right)]
-                trees[id].append(root)
-                return id
+    def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+        """
+        Idea:
+            For each unique tree, assign it a unique id which is equal to the number of unique registered at that time
+            By using a post order, the subtree can be represented as (val, left_id, right_id)
+            Use two dicts, one to store id of each subtree, one for storing nodes that have the same id
+        """
+        from collections import defaultdict
+        tree_ids = defaultdict()
+        id_node = defaultdict(list)
+        tree_ids.default_factory = tree_ids.__len__
 
-        trees = collections.defaultdict(list)
-        treeid = collections.defaultdict()
-        treeid.default_factory = treeid.__len__
-        getid(root)
-        return [roots[0] for roots in trees.values() if roots[1:]]
+        def post_order(node: TreeNode) -> int:
+            if node:
+                node_id = tree_ids[(node.val, post_order(node.left), post_order(node.right))]
+                id_node[node_id].append(node)
+                return node_id
+            return -1
+
+        post_order(root)
+        return [id_node[k][0] for k in id_node if len(id_node[k]) > 1]
 
 
 if __name__ == '__main__':
