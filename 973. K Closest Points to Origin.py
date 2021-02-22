6d22c84671e08afdddfbb6584448469ef1d38bb7
RockyZhenxiang <rockys910109@gmail.com>
2021-02-21 21:36:27 -0800

    973

--- 973. K Closest Points to Origin.py
+++ 973. K Closest Points to Origin.py
@@ -7,48 +7,32 @@ class Solution:
         return points[:K]
 
     def kClosest(self, points, K):
-        import random
-        dist = lambda i: points[i][0]**2 + points[i][1]**2
-
-        def sort(i, j, K):
-            # Partially sorts A[i:j+1] so the first K elements are
-            # the smallest K elements.
-            if i >= j: return
-
-            # Put random element as A[i] - this is the pivot
-            k = random.randint(i, j)
-            points[i], points[k] = points[k], points[i]
-
-            mid = partition(i, j)
-            if K < mid - i + 1:
-                sort(i, mid - 1, K)
-            elif K > mid - i + 1:
-                sort(mid + 1, j, K - (mid - i + 1))
-
-        def partition(i, j):
-            # Partition by pivot A[i], returning an index mid
-            # such that A[i] <= A[mid] <= A[j] for i < mid < j.
-            oi = i
-            pivot = dist(i)
-            i += 1
-
-            while True:
-                while i < j and dist(i) < pivot:
-                    i += 1
-                while i <= j and dist(j) >= pivot:
-                    j -= 1
-                if i >= j: break
-                points[i], points[j] = points[j], points[i]
-
-            points[oi], points[j] = points[j], points[oi]
-            return j
-
-        sort(0, len(points) - 1, K)
+        self.sort(points, 0, len(points) - 1, K)
         return points[:K]
+
+    def sort(self, points, l, r, K):
+        if l < r:
+            p = self.partition(points, l, r)
+            if p == K:
+                return
+            elif p < K:
+                self.sort(points, p + 1, r, K)
+            else:
+                self.sort(points, l, p - 1, K)
+
+    def partition(self, points, l, r):
+        pivot = points[r]
+        a = l
+        for i in range(l, r):
+            if (points[i][0] ** 2 + points[i][1] ** 2) <= (pivot[0] ** 2 + pivot[1] ** 2):
+                points[a], points[i] = points[i], points[a]
+                a += 1
+        points[a], points[r] = points[r], points[a]
+        return a
 
 
 if __name__ == '__main__':
-    points = [[1, 3], [-2, 2]]
+    points = [[2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [2, 2], [1, 1]]
     K = 1
     sol = Solution()
-    print(sol.kClosest_naive(points, K))
+    print(sol.kClosest(points, K))
