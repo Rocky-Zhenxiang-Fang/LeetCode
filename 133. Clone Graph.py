ebc84f2398a168f89c1c0ae56f242e9d543db33f
RockyZhenxiang <rockys910109@gmail.com>
2021-03-05 14:26:01 -0800

    44

--- 133. Clone Graph.py
+++ 133. Clone Graph.py
@@ -1,13 +1,35 @@
-
 # Definition for a Node.
 class Node:
-    def __init__(self, val = 0, neighbors = None):
+    def __init__(self, val=0, neighbors=None):
         self.val = val
-        self.neighbors = neighbors if neighbors is not None else [] # List[Node]
-
+        self.neighbors = neighbors if neighbors is not None else []  # List[Node]
 
 
 class Solution:
+
     def cloneGraph(self, node: 'Node') -> 'Node':
+        """
+        Idea:
+            Do a DFS:
+                For each neighbors:
+                    if neighbor node have not be created:
+                        create the node
+                    link node and neighbor together
+        """
+        copy_map = {node: Node(node.val)}
+        visited = set()
+        stack = [node]
+        while stack:
+            curr = stack.pop()
+            if curr not in visited:
+                visited.add(curr)
+                for nei in curr.neighbors:
+                    if nei not in copy_map:
+                        copy_map[nei] = Node(nei.val)
+                    copy_map[nei].neighbors.append(copy_map[curr])
+                    copy_map[curr].neighbors.append(copy_map[nei])
+        return copy_map[node]
+
+
 
 
